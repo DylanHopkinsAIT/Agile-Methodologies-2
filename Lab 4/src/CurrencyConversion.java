64443d592e803ced6c2b581cbdc7a26dff8b933f
Dylan Hopkins <90385685+DylanHopkinsAIT@users.noreply.github.com>
2022-02-14 13:47:17 +0000

    Lab 4 additional test

--- Lab 4/src/CurrencyConversion.java
+++ Lab 4/src/CurrencyConversion.java
@@ -1,28 +1,35 @@
 public class CurrencyConversion {
 
-    public double ConvertCurrency(double usdAmount, double conversionRate){
+    public double ConvertCurrency(double usdAmount, double conversionRate) {
 
-        try{
-            if (isPositive(usdAmount) && isPositive(conversionRate)){
+        try {
+            if (isPositive(usdAmount) && isWithinBounds(usdAmount) && isPositive(conversionRate)) {
                 return usdAmount * conversionRate;
-            }
-            else{
-                throw new IllegalArgumentException("Please only enter positive number values...");
+            } else {
+                throw new IllegalArgumentException("Please only enter positive number values \n and dollar values between 100 and 100000...");
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return -1;
         }
 
     }
 
-    private boolean isPositive(double input){
+    private boolean isPositive(double input) {
         boolean isPositive;
 
         isPositive = input >= 0;
 
-        return  isPositive;
+        return isPositive;
+    }
+
+    private boolean isWithinBounds(double usdAmount) {
+        boolean isWithinBounds;
+
+        isWithinBounds = usdAmount > 100 && usdAmount < 100000;
+
+        return isWithinBounds;
     }
 
 }
--- Lab 4/src/CurrencyConversionTest.java
+++ Lab 4/src/CurrencyConversionTest.java
@@ -5,18 +5,27 @@ public class CurrencyConversionTest extends TestCase {
     CurrencyConversion cc = new CurrencyConversion();
 
     //Test number: 1
-    //Test objective: Reject invalid inputs
-    //Test input(s): currencyAmount = -10, conversionRate = 10"
-    //Test expected output(s): -1
+    //Test objective: Reject negative inputs
+    //Test input(s): usdAmount = -1000, conversionRate = -0.88"
+    //Test expected output(s): -1.0
     public void testCurrencyConversion001() {
         assertEquals(-1.0, cc.ConvertCurrency(-10, 10));
     }
 
     //Test number: 2
     //Test objective: Verify correct conversion result
-    //Test input(s): currencyAmount = 67.80, conversionRate = 0.33
+    //Test input(s): usdAmount = 67.80, conversionRate = 0.33
     //Test expected output(s): 23.37
     public void testCurrencyConversion002() {
-        assertEquals(22.37, cc.ConvertCurrency(67.80, 0.33), 0.1);
+        assertEquals(880.00, cc.ConvertCurrency(1000, 0.88), 0.1);
     }
+
+    //Test number: 3
+    //Test objective: Reject inputs outside of boundary limits
+    //Test input(s): usdAmount = 100000 , conversionRate = 0.88
+    //Test expected output(s):
+    public void testCurrencyConversion003(){
+        assertEquals(-1, cc.ConvertCurrency(100000,0.88),0.1);
+    }
+
 }
